d7b3e68e21ecce9b0b0ed12f784b165deafce6bb
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-01-29 02:33:06 -0300

    add searchDishes service and clean the console log

--- src/services/items.js
+++ src/services/items.js
@@ -4,9 +4,21 @@ import axios from 'axios';
 export async function getDishes() {
   try {
     const response = await axios.get(
-      'https://api.spoonacular.com/recipes/complexSearch?apiKey=e5efa520d806449eb6185b9de8fdcf64'
+      'https://api.spoonacular.com/recipes/complexSearch?apiKey=e5efa520d806449eb6185b9de8fdcf64&addRecipeInformation=true'
     );
-    console.log(response.data.results, 'response');
+    console.log('data', response.data.results);
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function searchDishes(query = '') {
+  try {
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=e5efa520d806449eb6185b9de8fdcf64&addRecipeInformation=true&query=${query}`
+    );
+    console.log('data', response.data.results);
     return response.data.results;
   } catch (error) {
     console.error(error);
