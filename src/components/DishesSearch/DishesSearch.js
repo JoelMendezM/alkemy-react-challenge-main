c4cc11d47b692d9846ef6429dd25c6ff9e010154
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-01-29 02:40:40 -0300

    delete the dataTest, change the validate logic, add query search

--- src/components/DishesSearch/DishesSearch.js
+++ src/components/DishesSearch/DishesSearch.js
@@ -1,81 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Formik } from 'formik';
 import { SearchFormContainer } from '../StyleComponents/Style';
-import { getDishes } from '../../services/items'
-import { Item } from '../Item/Item'
-import { Home } from '../Home/Home'
+import { useMenu } from '../../context/MenuContext';
+import { Label } from '../StyleComponents/Style';
 
 export const DishesSearch = () => {
-  const [foundDishes, setFoundDishes] = useState([]);
-  const data = [
-      {
-        id: 0,
-        title: 'Cauliflower, Brown Rice',
-        image: 'pic1'
-      },
-      {
-        id: 2,
-        title: 'Homemade Garlic and',
-        image: 'pic2'
-      },
-      {
-        id: 3,
-        title: 'Berry Banana Breakfast',
-        image: 'pic3'
-      },
-      {
-        id: 4,
-        title: 'Garlicky Kale',
-        image: 'pic4'
-      }
-    ]
+  const { searchDishesByQuery } = useMenu();
 
   return (
     <>
       <Formik
         initialValues={{searchValue: ''}}
-        validate={(inputValue) => {
-          
-          if(inputValue.searchValue.length >= 2) {
-              let test = [];
-                
-                console.log('searchingTest :>> ', inputValue.searchValue);
-
-                test = data.filter((match) => {
-                  if (match.title.toLowerCase().includes(inputValue.searchValue)) {
-                    test.push(match);
-                  }
-                    return setFoundDishes(test);
-                })
-            }
-
-            /*if(inputValue.searchValue.length > 1) {
-                getDishes()
-                .then((response) => {
-                    console.log('response :>> ', response);
-                })
-            }*/
-            
-
-
+        
+        onSubmit={(values, { resetForm }) => {
+          if(values.searchValue.length >= 2) {
+            let query = values.searchValue.toLowerCase();
+            searchDishesByQuery(query);
+          }
+          resetForm();
         }}
       >
         {({handleChange, handleSubmit, values})=> (
           <div>
             <SearchFormContainer onSubmit={handleSubmit}>
-              <label className='form-label'>Buscador de platos</label>
+              <Label className='form-label'>Buscador de platos: </Label>
               <input
               type='text'
               id='searching'
               name='searchValue'
               value={values.searchValue}
+              placeholder='ej: Broccoli'
               onChange={handleChange}/>
             </SearchFormContainer>
-
-            {values.searchValue.length >= 2 ?
-              <Item dishes={foundDishes}/> :
-              <Home />
-            }
           </div>
         )}
       </Formik>
