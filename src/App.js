13920c903b1c7013711f154dc09b4e294d5aa9f4
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-01-29 02:37:21 -0300

    changing the render logic and cleaning the component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@ import { Login } from './components/Login/Login';
 import { NavBar } from './components/NavBar/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home';
-import { DishesSearch } from './components/DishesSearch/DishesSearch';
-
-// APIKEY: bab401f1a68946959bb8cd42dba16eda
-// APIKEY2: e5efa520d806449eb6185b9de8fdcf64
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <DishesSearch/>
       <Routes>
-        {/* <Route path="/" element={localStorage.length === 0 ? <Login /> : <Home />} /> */}
-        {/* <Route path="/" element={<DishesSearch/>} /> */}
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/home" element={localStorage.length === 0 ? <Login /> : <Home />} /> */}
-        {/* <Route path="/home" element={<Home />} /> */}
-        <Route
-          path="/acceleration"
-          element={localStorage.length === 0 ? <Login /> : <div>ACELARATE</div>}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </Router>
   );
