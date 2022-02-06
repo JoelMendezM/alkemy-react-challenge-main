7ba2ec711eb4b57381b3df1cf4a0de0225c077e8
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-02-05 21:26:24 -0300

    logic to disappear some paragrah in the menu

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,31 +4,36 @@ import { DishesContainer, CardDish, Button } from '../StyleComponents/Style';
 import className from 'classnames';
 import { NavLink } from 'react-router-dom';
 
-
-export const Item = ( {dishes, appearButton = {'d-none': false}, disapeartButton = {'d-none': true}} ) => {
+export const Item = ( {
+  dishes, 
+  appearButton = {'d-none': false},
+  disapeartButton = {'d-none': true},
+  disapearParagrah = {'d-none': false}
+  } ) => {
   const { onAddToMenu, removeDish } = useMenu();
+  const wraperParagrah = className(disapearParagrah);
   const wraperrClassesDetailButton = className('btn btn-primary', disapeartButton);
   const wraperrClassesDeleteButton = className('btn btn-danger', disapeartButton);
   const wraperrClassesAddMenuButton = className('btn btn-primary', appearButton);
-  console.log('dishes :>> ', dishes);
+
   return (
     <>
       <DishesContainer>
-          {dishes && dishes.length > 0 && dishes.map((dishes) => (
-            <div key={dishes.id}>
+          {dishes && dishes.length > 0 && dishes.map((dish) => (
+            <div key={dish.id}>
               <CardDish className="card" style={{width: "18rem"}}>
-                <img src={dishes.image} className="card-img-top" alt="..."/>
+                <img src={dish.image} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">{dishes.title}</h5>
-                  <p className="card-text"><b>Precio por porción: </b>{dishes.pricePerServing}</p>
-                  <p className="card-text"><b>Tiempo de preparación (min): </b>{dishes.readyInMinutes}</p>
-                  <p className="card-text"><b>Health Score: </b>{dishes.healthScore}</p>
-                  <p className="card-text"><b>Apto vegano: </b>{dishes.vengan === true ? 'si' : 'no'}</p>
-                  <p className="card-text"><b>Vegetariano: </b>{dishes.vegetarian === true ? 'si' : 'no'}</p>
-                  <p className="card-text"><b>Apto celíaco: </b>{dishes.glutenFree === true ? 'si' : 'no'}</p>
-                  <NavLink to={`/itemDetail/${dishes.id}`} className={wraperrClassesDetailButton} >Detalle del plato</NavLink>
-                  <Button className={wraperrClassesDeleteButton} onClick={() => removeDish()}>Eliminar</Button>
-                  <Button className={wraperrClassesAddMenuButton} onClick={() => onAddToMenu(dishes)}>Añadir al menú</Button>
+                  <h5 className="card-title">{dish.title}</h5>
+                  <p className={wraperParagrah}><b>Precio por porción: </b>{dish.pricePerServing}</p>
+                  <p className={wraperParagrah}><b>Tiempo de preparación (min): </b>{dish.readyInMinutes}</p>
+                  <p className={wraperParagrah}><b>Health Score: </b>{dish.healthScore}</p>
+                  <p className="card-text"><b>Apto vegano: </b>{dish.vengan === true ? 'si' : 'no'}</p>
+                  <p className="card-text"><b>Vegetariano: </b>{dish.vegetarian === true ? 'si' : 'no'}</p>
+                  <p className="card-text"><b>Apto celíaco: </b>{dish.glutenFree === true ? 'si' : 'no'}</p>
+                  <NavLink to={`/item/detail/${dish.id}`} className={wraperrClassesDetailButton} >Detalle del plato</NavLink>
+                  <Button className={wraperrClassesDeleteButton} onClick={() => removeDish(dish.id)}>Eliminar</Button>
+                  <Button className={wraperrClassesAddMenuButton} onClick={() => onAddToMenu(dish)}>Añadir al menú</Button>
                 </div>
               </CardDish>
             </div>
