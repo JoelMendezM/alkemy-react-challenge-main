6552c08d9db17629c8d2f50878b7bd941509e68b
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-02-05 21:28:26 -0300

    creating average function and average logic

--- src/context/MenuContext.js
+++ src/context/MenuContext.js
@@ -15,11 +15,7 @@ const useMenu = () => {
 const MenuProvider = ({children}) => {
   const [menu, setMenu] = useState([]);
   const [dishesList, setDishesList] = useState([]);
-<<<<<<< HEAD
-  const [averagePrice, setAveragePrice] = useState(0);
-=======
   const [dish, setDish] = useState([]);
->>>>>>> a5c2e6398f6e10447b306715da9be4e9b92ab9ef
   
   console.log('menu :>> ', menu);
 
@@ -45,77 +41,45 @@ const MenuProvider = ({children}) => {
       })
   }
 
-<<<<<<< HEAD
-  const averageMenuPrice = () => {
-    let menuPrice = 0;
-    let quantityItems = 0;
-      menuPrice = menu.map((item) => {
-        console.log('item.price :>> ', item.price);
-        quantityItems = quantityItems + 1;
-        menuPrice = menuPrice + item.price;
-        let average = menuPrice / quantityItems;
-        console.log('average :>> ', average);
-
-      return average
-    })
-  }
-  const averageMenuPreparation = () => {
-    let menuTimePreparation = 0;
-    let quantityItems = 0;
-    menuTimePreparation = menu.map((item) => {
-        console.log('item.price :>> ', item.timePreparation);
-        quantityItems = quantityItems + 1;
-        menuTimePreparation = menuTimePreparation + item.timePreparation;
-        let average = menuTimePreparation / quantityItems;
-        console.log('average :>> ', average);
-
-      return average
-    })
-  }
-  const averageMenuHealthScore = () => {
-    let menuHealthScore = 0;
-    let quantityItems = 0;
-      menuHealthScore = menu.map((item) => {
-        quantityItems = quantityItems + 1;
-        menuHealthScore = menuHealthScore + item.healthScore;
-        let average = menuHealthScore / quantityItems;
-        console.log('average :>> ', average);
-
-      return average
-    })
-=======
   const getDishById = (id) => {
     getDish(id)
       .then((res) => {
         setDish(res);
       })
->>>>>>> a5c2e6398f6e10447b306715da9be4e9b92ab9ef
   }
 
-  const onAddToMenu = (dishes) => {
-    const dishAddToMenu = {
-        id: dishes.id,
-        title: dishes.title,
-        image: dishes.image,
-        vegan: dishes.vegan,
-        vegetarian: dishes.vegetarian,
-        celiac: dishes.glutenFree,
-        price: dishes.pricePerServing,
-        timePreparation: dishes.readyInMinutes,
-        healthScore: dishes.healthScore,
-        summary: dishes.summary
+  const onAddToMenu = (dish) => {
+    let isANewDish = true;
+
+    menu.forEach((element) => {
+      if (element.id === dish.id) {
+        isANewDish = false;
       }
+    });
 
-    setMenu((previousState) => [...previousState, dishAddToMenu]);
-  }
+    if (!isANewDish) {
+      return;
+    }
 
-  const removeDish = (index) => {
-    menu.splice(index, 1);
+    const newDish = {
+      id: dish.id,
+      title: dish.title,
+      image: dish.image,
+      vegan: dish.vegan,
+      vegetarian: dish.vegetarian,
+      celiac: dish.glutenFree,
+      price: dish.pricePerServing,
+      timePreparation: dish.readyInMinutes,
+      healthScore: dish.healthScore,
+      summary: dish.summary
+    }
+    console.log('agregue un nuevo plato :>> ',  newDish);
+    setMenu((previousState) => [...previousState, newDish]);
+  };
 
-    let updatingMenu = menu.map((dish) => {
-      return {...dish}
-    })
-    setMenu(updatingMenu);
+  const removeDish = (id) => {
+    const newMenu = menu.filter((dish) => dish.id !== id)
+    setMenu(newMenu);
    }
 
   return (
@@ -127,14 +91,8 @@ const MenuProvider = ({children}) => {
         dishesList,
         getDishesList,
         searchDishesByQuery,
-<<<<<<< HEAD
-        averageMenuPrice,
-        averageMenuPreparation,
-        averageMenuHealthScore
-=======
         getDishById,
-        dish
->>>>>>> a5c2e6398f6e10447b306715da9be4e9b92ab9ef
+        dish,
         }}>
         {children}
       </MenuContext.Provider>
